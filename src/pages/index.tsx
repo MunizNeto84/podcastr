bfa757fff664c25c13374571f1026742c1a70e87
Muniz Neto <muniznetto@outlook.com>
2021-04-23 13:12:21 -0300

    page routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { format, parseISO} from 'date-fns'
 import { enUS } from 'date-fns/locale'
 import { api } from '../service/api'
 import { convertDurationToTimeSting } from '../util/covertDurationToTimeString'
+import Link from 'next/link'
 
 import styles from './home.module.scss'
 
@@ -11,7 +12,6 @@ type Episode = {
     id: string;
     title: string;
     thumbnail: string;
-    description: string;
     members: string;
     duration: number;
     durationAsString: string;
@@ -42,7 +42,9 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
                   />
 
                   <div className={styles.episodesDetails}>
-                    <a href=""> { episode.title }</a>
+                    <Link href={`/episodes/${episode.id}`}>
+                    <a> { episode.title }</a>
+                    </Link>
                     <p>{episode.members}</p>
                     <span>{episode.publishedAt}</span>
                     <span>{episode.durationAsString}</span>
@@ -62,12 +64,14 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
 
             <table cellSpacing={0}>
               <thead>
-                <th></th>
-                <th>Podcast</th>
-                <th>Members</th>
-                <th>Date</th>
-                <th>Duration</th>
-                <th></th>
+                <tr>
+                  <th></th>
+                  <th>Podcast</th>
+                  <th>Members</th>
+                  <th>Date</th>
+                  <th>Duration</th>
+                  <th></th>
+                </tr>
               </thead>
               <tbody>
                 {allEpisodes.map(episode => {
@@ -83,7 +87,9 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
                         />
                       </td>
                       <td>
-                        <a href="">{episode.title}</a>
+                        <Link href={`/episodes/${episode.id}`}>
+                          <a>{episode.title}</a>
+                        </Link>
                       </td>
                       <td>{episode.members}</td>
                       <td style={{width: 100}}>{episode.publishedAt}</td>
@@ -121,7 +127,6 @@ export const getStaticProps: GetStaticProps = async () => {
       publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: enUS}),
       duration: Number(episode.file.duration),
       durationAsString: convertDurationToTimeSting(Number(episode.file.duration)),
-      description: episode.description,
       url: episode.file.url,
     }
   })
